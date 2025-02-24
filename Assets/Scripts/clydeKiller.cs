3307bb605a57b2eb73c5418ff8c34676a733217c
CMartie <cayceemartindale@lcad.edu>
2025-02-24 15:18:47 -0800

    continued move scripts and new scene

--- Assets/Scripts/clydeKiller.cs
+++ Assets/Scripts/clydeKiller.cs
@@ -1,11 +1,18 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class clydeKiller : MonoBehaviour
 {
+    public List<clydeMoveForce> clydes = new List<clydeMoveForce>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int clydesThatDie = Random.Range(0, clydes.Count);
+        //clydes[clydesThatDie].Explode();
+        clydes.RemoveAt(clydesThatDie);
+
     }
 
     // Update is called once per frame
@@ -13,4 +20,10 @@ public class clydeKiller : MonoBehaviour
     {
         
     }
+
+    public void Explode()
+    {
+        Destroy(this.gameObject);
+    }
+
 }
